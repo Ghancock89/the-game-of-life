2d6a7a038af65399533b94d6ee9d344fdcb63350
ghanc <ghancock@live.co.uk>
2021-07-03 12:50:17 +0100

    Finished up until scenario 4

---
+++ spec/game-of-life.spec.js
@@ -0,0 +1,86 @@
+const GameOfLife = require("../../game-of-life/src/game-of-life");
+
+describe("Game of Life", () => {
+
+    describe("Scenario 0", () => {
+        it("should return a grid of dead cells when given a grid of dead cells", () => {
+            const currentGrid = [["0", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]];
+            /*
+                ---------
+                |0||0||0|
+                |0||0||0|
+                |0||0||0|
+                ---------
+             */
+            const game = new GameOfLife();
+            const result = game.play(currentGrid);
+            expect(result).toEqual(currentGrid);
+        });
+    });
+
+    describe("Scenario 1", () => {
+        it("should kill live cells when less than two neighbours", () => {
+            const currentGrid = [["0", "1", "0"], ["0", "1", "0"], ["0", "0", "0"]];
+            /*
+                ---------
+                |0||1||0|
+                |0||1||0|
+                |0||0||0|
+                ---------
+             */
+            const game = new GameOfLife();
+            const result = game.play(currentGrid);
+            expect(result).toEqual([["0", "0", "0"], ["0", "0", "0"], ["0", "0", "0"]]);
+        });
+    });
+
+    describe("Scenario 2", () => {
+        it("should kill live cells when more than three neighbours", () => {
+            const currentGrid = [["1", "1", "1"], ["0", "1", "0"], ["1", "0", "0"]];
+            /*
+                ---------
+                |1||1||1|
+                |0||1||0|
+                |1||0||0|
+                ---------
+             */
+            const game = new GameOfLife();
+            const result = game.play(currentGrid);
+            expect(result).toEqual([["1", "1", "1"], ["0", "0", "1"], ["0", "0", "0"]]);
+        });
+    });
+
+    describe("Scenario 3", () =>{
+        it("A live cell should stay alive if it has 2 or 3 neighbours", () => {
+            const currentGrid = [["0", "1", "1"], ["0", "1", "1"], ["0", "0", "0"]];
+            /*
+                ---------
+                |0||1||1|
+                |0||1||1|
+                |0||0||0|
+                ---------
+             */
+            const game = new GameOfLife();
+            const result = game.play(currentGrid);
+            expect(result).toEqual([["0", "1", "1"], ["0", "1", "1"], ["0", "0", "0"]]);
+        });
+    });
+
+    describe("Scenario 4", () => {
+        it("If an empty position has exactly 3 neighbours, a live cell should be created in the empty position", () => {
+            const currentGrid = [["0", "1", "1"], ["0", "1", "0"], ["0", "0", "0"]];
+            /*
+                ---------
+                |0||1||1|
+                |0||1||0|
+                |0||0||0|
+                ---------
+             */
+            const game = new GameOfLife();
+            const result = game.play(currentGrid);
+            expect(result).toEqual([["0", "1", "1"], ["0", "1", "1"], ["0", "0", "0"]]);
+        })
+    })
+
+
+});
---
+++ src/game-of-life.js
@@ -0,0 +1,47 @@
+class GameOfLife {
+    //keys: 0 = dead, 1 = alive
+
+    play(seedGrid) {
+        const newGrid = JSON.parse(JSON.stringify(seedGrid));
+        for(let x = 0; x < newGrid.length; x++){
+            for(let y = 0; y < newGrid[x].length; y++){
+                if(seedGrid[x][y] === '1'){
+                    const neighbourCount = this.getNeighbourCount(x, y, seedGrid);
+                    if(neighbourCount < 2 || neighbourCount > 3){
+                        newGrid[x][y] = '0';
+                    }
+                } else {
+                    const neighbourCount = this.getNeighbourCount(x, y, seedGrid);
+                    if(neighbourCount === 3){
+                        newGrid[x][y] = '1';
+                    }
+                }
+            }
+        }
+        return newGrid;
+
+    }
+
+
+    getNeighbourCount(x, y, seedGrid){
+        const checks = [[x-1, y-1],[x,y-1],[x+1, y-1],[x-1,y], [x+1, y],[x-1, y+1], [x, y+1], [x+1, y+1]];
+        let neighbourCount = 0;
+        try{
+
+            for(let i = 0; i < checks.length; i++){
+                if(checks[i][0] > -1 && checks[i][1] > -1 && checks[i][0] < 3 && checks[i][1] < 3) {
+                    const cx = checks[i][0];
+                    const cy = checks[i][1];
+                    if (seedGrid[cx][cy] !== '0') {
+                        neighbourCount++;
+                    }
+                }
+            }
+        }catch (e) {
+
+        }
+        return neighbourCount;
+    }
+}
+
+module.exports = GameOfLife;
